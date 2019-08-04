e723a413f9b135930f9f101b0a00620b72a3704a
芦原亨 <dennkidaisuki1007@gmail.com>
2019-08-04 23:34:48 +0900

    マイグレーション編集(null禁止)

--- db/migrate/20190804084335_create_items.rb
+++ db/migrate/20190804084335_create_items.rb
@@ -1,12 +1,12 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-      t.string :name
-      t.text :description
-      t.integer :price
-      t.string :seller
-      t.string :email
-      t.text :image_url
+      t.string :name , null: false
+      t.text :description , null: false
+      t.integer :price  , null: false
+      t.string :seller , null: false
+      t.string :email , null: false
+      t.text :image_url , null: false
 
       t.timestamps
     end
